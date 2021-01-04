c07f2e45a72910b01978953b2f7d87b36e9050c7
Mitra Nami <a.mxnami@gmail.com>
2021-01-04 01:51:55 -0500

    Remove the task from the localStorage

--- helper.js
+++ helper.js
@@ -1,9 +1,20 @@
 // Adds the value to the array associated with the key name in the localStorage
-const persist = function(name, value) {
+const persistInLocalStorage = function(name, value) {
   if (!localStorage.getItem(name)) {
     localStorage.setItem(name, '[]');
   }
   const values = JSON.parse(localStorage.getItem(name));
   values.push(value);
   localStorage.setItem(name, JSON.stringify(values));
+};
+
+
+// Remove the given value from the array associated with the key name in the localStorage
+const removeFromLocalStorage = function(name, value) {
+  if (!localStorage.getItem(name)) {
+    localStorage.setItem(name, '[]');
+  }
+  const values = JSON.parse(localStorage.getItem(name));
+  const newValues = values.filter(val => val !== value);
+  localStorage.setItem(name, JSON.stringify(newValues));
 };
